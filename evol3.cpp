766527482c9b3f3b4d60da14c4510eeea634a082
hskalin <49724493+hskalin@users.noreply.github.com>
2019-07-29 03:39:48 +0000

    added getch

--- evol3.cpp
+++ evol3.cpp
@@ -9,6 +9,9 @@
 #include <atomic>
 #include <random>
 
+#include <unistd.h>
+#include <termios.h>
+
 #define WIDTH           165
 #define HEIGHT          35
 #define PLANT_ENERGY    80
@@ -42,6 +45,26 @@
 #endif  /* _COLORS_ */
 
 //using namespace std;
+
+char getch() {
+        char buf = 0;
+        struct termios old = {0};
+        if (tcgetattr(0, &old) < 0)
+                perror("tcsetattr()");
+        old.c_lflag &= ~ICANON;
+        old.c_lflag &= ~ECHO;
+        old.c_cc[VMIN] = 1;
+        old.c_cc[VTIME] = 0;
+        if (tcsetattr(0, TCSANOW, &old) < 0)
+                perror("tcsetattr ICANON");
+        if (read(0, &buf, 1) < 0)
+                perror ("read()");
+        old.c_lflag |= ICANON;
+        old.c_lflag |= ECHO;
+        if (tcsetattr(0, TCSADRAIN, &old) < 0)
+                perror ("tcsetattr ~ICANON");
+        return (buf);
+}
 
 using std::cout;
 using std::cin;
@@ -290,7 +313,7 @@ void fun(){
     
     while(!terminate.load()){
         
-        cin>>command;
+        command = getch();
         if(command=='x'){
             end = 1;
             break;
